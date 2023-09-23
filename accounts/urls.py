1d671aa77a03ed7b5f853c5ffa6618efbd31dc22
bigtorresg <bigtorresg@gmail.com>
2023-09-23 20:11:18 +0300

    email confirmation fixed response messages added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include, re_path
 from allauth.account.views import confirm_email
+from .views import CustomEmailConfirmView
 
 urlpatterns = [
     path("", include("dj_rest_auth.urls")),
     re_path(
-        r"^rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$",
-        confirm_email,
+        r"^registration/account-confirm-email/(?P<key>[-:\w]+)/$",
+        CustomEmailConfirmView.as_view(),
         name="account_confirm_email",
     ),
-    path(r"rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(r"registration/", include("dj_rest_auth.registration.urls")),
 ]
